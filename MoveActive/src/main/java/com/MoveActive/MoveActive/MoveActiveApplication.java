e2b93d6e233da9a8cea4540bcecc41b1b406b39e
Luis Acuña <luisandresverdugox@gmail.com>
2022-11-25 19:23:49 -0500

    Subiendo primer código de microservicio de Movimiento Activo

---
+++ MoveActive/src/main/java/com/MoveActive/MoveActive/MoveActiveApplication.java
@@ -0,0 +1,13 @@
+package com.MoveActive.MoveActive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveActiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveActiveApplication.class, args);
+	}
+
+}
---
+++ MoveActive/src/test/java/com/MoveActive/MoveActive/MoveActiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoveActive.MoveActive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveActiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
