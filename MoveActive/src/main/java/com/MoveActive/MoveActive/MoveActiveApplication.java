5b57b82e45e09d867342e4ee5c7a4acf7a03c5df
verdugox <luisandresverdugox@gmail.com>
2022-12-13 23:19:19 -0500

    Subiendo Configuraciones de Docker y Eureka

--- MoveActive/src/main/java/com/MoveActive/MoveActive/MoveActiveApplication.java
+++ MoveActive/src/main/java/com/MoveActive/MoveActive/MoveActiveApplication.java
@@ -2,8 +2,10 @@ package com.MoveActive.MoveActive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MoveActiveApplication {
 
 	public static void main(String[] args) {
